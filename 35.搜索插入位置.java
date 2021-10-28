aa341732f39afd533c7672e0735a4d0b621d146b
cc13446 <42083585+cc13446@users.noreply.github.com>
2021-10-28 22:41:35 +0800

    35

--- 35.搜索插入位置.java
+++ 35.搜索插入位置.java
@@ -7,7 +7,23 @@
 // @lc code=start
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        return helper(nums, 0, nums.length - 1, target);
+    }
 
+    public int helper(int[] nums, int left, int right, int target) {
+        if (left >= right) {
+            if (target > nums[left]) return left + 1;
+            else if (target < nums[left]) return left;
+            else return left;
+        }
+        int mid = (left + right) / 2;
+        if (nums[mid] > target) {
+            return helper(nums, left, mid - 1, target);
+        } else if (nums[mid] < target) {
+            return helper(nums, mid + 1, right, target);
+        } else {
+            return mid;
+        }
     }
 }
 // @lc code=end

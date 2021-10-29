41f76337f91f76e511d9b153dc4316d83ff8220d
cc13446 <1344613920@qq.com>
2021-10-29 17:39:09 +0800

    定义

--- 110.平衡二叉树.java
+++ 110.平衡二叉树.java
@@ -5,21 +5,20 @@
  */
 
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+ 
 class Solution {
     public boolean isBalanced(TreeNode root) {
         return height(root) >= 0;
---
+++ 118.杨辉三角.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=118 lang=java
+ *
+ * [118] 杨辉三角
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (numRows == 0) return res;
+        List<Integer> pre = new LinkedList<>();
+        pre.add(1);
+        res.add(pre);
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> temp = new LinkedList<>();
+            temp.add(1);
+            for (int j = 0; j < pre.size() - 1; i++) {
+                temp.add(pre.get(j) + pre.get(j + 1));
+            }
+            temp.add(1);
+            pre = temp;
+            res.add(pre);
+        }
+        return res;
+    }
+}
+// @lc code=end
+

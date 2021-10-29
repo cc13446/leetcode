a9e60f780ba08ebca2a2c40473e260b7018e3c68
cc13446 <1344613920@qq.com>
2021-10-29 17:21:14 +0800

    112

---
+++ 112.路径总和.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=112 lang=java
+ *
+ * [112] 路径总和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        else if (root.left == null && root.right == null && targetSum == root.val) return true;
+        else if (root.left == null && root.right != null) return hasPathSum(root.right, targetSum - root.val);
+        else if (root.left != null && root.right == null) return hasPathSum(root.left, targetSum - root.val);
+        else if (root.left != null && root.right != null) 
+            return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+        else return false;
+    }
+}
+// @lc code=end
+

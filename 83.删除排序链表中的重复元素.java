9dd0a5fa438bf97e89e1dfc2ad673960a7e34978
cc13446 <1344613920@qq.com>
2021-10-29 17:40:54 +0800

    定义

--- 83.删除排序链表中的重复元素.java
+++ 83.删除排序链表中的重复元素.java
@@ -7,16 +7,14 @@ import java.util.List;
  */
 
 // @lc code=start
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode pre = head;

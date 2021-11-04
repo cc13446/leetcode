e713f5f0b9c5e8ed2d6251e5c6ac274abf058048
cc13446 <42083585+cc13446@users.noreply.github.com>
2021-11-04 08:55:06 +0800

    190

---
+++ 190.颠倒二进制位.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=190 lang=java
+ *
+ * [190] 颠倒二进制位
+ */
+
+// @lc code=start
+class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int rev = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            rev |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return rev;
+    }
+}
+// @lc code=end
+

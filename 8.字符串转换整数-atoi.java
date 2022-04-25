3340d6cfbe326af3b0ba98f72f62b2940f29a3fe
cc13446 <42083585+cc13446@users.noreply.github.com>
2022-04-25 13:28:12 +0800

    8

---
+++ 8.字符串转换整数-atoi.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=8 lang=java
+ *
+ * [8] 字符串转换整数 (atoi)
+ */
+
+// @lc code=start
+class Solution {
+    public int myAtoi(String str) {
+        str = str.trim();
+        if (str.length() == 0) return 0;
+        if (!Character.isDigit(str.charAt(0)) && str.charAt(0) != '-' && str.charAt(0) != '+') {
+            return 0;
+        }
+        int ans = 0;
+        boolean neg = str.charAt(0) == '-';
+        int i = !Character.isDigit(str.charAt(0)) ? 1 : 0;
+        while (i < str.length() && Character.isDigit(str.charAt(i))) {
+            int tmp = ((neg ? Integer.MIN_VALUE : Integer.MIN_VALUE + 1) + (str.charAt(i) - '0')) / 10;
+            if (tmp > ans) {
+                return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+            ans = ans * 10 - (str.charAt(i++) - '0');
+        }
+        return neg ? ans : -ans;
+    }
+}
+// @lc code=end
+

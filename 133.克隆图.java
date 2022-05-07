4196488072bbf96d7c4161e66e36dc87f33cef3e
cc13446 <42083585+cc13446@users.noreply.github.com>
2022-05-07 11:09:33 +0800

    133

---
+++ 133.克隆图.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+/*
+ * @lc app=leetcode.cn id=133 lang=java
+ *
+ * [133] 克隆图
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return node;
+        }
+
+        HashMap<Node, Node> visited = new HashMap<>();
+
+        // 将题目给定的节点添加到队列
+        LinkedList<Node> queue = new LinkedList<Node> ();
+        queue.add(node);
+        // 克隆第一个节点并存储到哈希表中
+        visited.put(node, new Node(node.val, new ArrayList<>()));
+
+        // 广度优先搜索
+        while (!queue.isEmpty()) {
+            // 取出队列的头节点
+            Node n = queue.remove();
+            // 遍历该节点的邻居
+            for (Node neighbor: n.neighbors) {
+                if (!visited.containsKey(neighbor)) {
+                    // 如果没有被访问过，就克隆并存储在哈希表中
+                    visited.put(neighbor, new Node(neighbor.val, new ArrayList<>()));
+                    // 将邻居节点加入队列中
+                    queue.add(neighbor);
+                }
+                // 更新当前节点的邻居列表
+                visited.get(n).neighbors.add(visited.get(neighbor));
+            }
+        }
+
+        return visited.get(node);
+    }
+}
+// @lc code=end
+

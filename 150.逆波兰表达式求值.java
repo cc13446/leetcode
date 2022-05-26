ffa0f5c449c280312c07022539768e6d1586a322
cc13446 <42083585+cc13446@users.noreply.github.com>
2022-05-26 15:49:48 +0800

    150 后缀表达式

---
+++ 150.逆波兰表达式求值.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode.cn id=150 lang=java
+ *
+ * [150] 逆波兰表达式求值
+ */
+
+// @lc code=start
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < tokens.length; i++) {
+            String cur = tokens[i];
+            if (cur.equals("+")) {
+                int right = s.pop();
+                int left = s.pop();
+                s.push(left + right);
+            } else if (cur.equals("-")) {
+                int right = s.pop();
+                int left = s.pop();
+                s.push(left - right);
+            } else if (cur.equals("*")) {
+                int right = s.pop();
+                int left = s.pop();
+                s.push(left * right);
+            } else if (cur.equals("/")) {
+                int right = s.pop();
+                int left = s.pop();
+                s.push(left / right);
+            } else {
+                s.push(Integer.parseInt(cur));
+            }
+        }
+        return s.pop();
+    }
+}
+// @lc code=end
+

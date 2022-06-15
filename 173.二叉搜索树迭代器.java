9aee7b4ca0512c360c05b3fee4777298228de39b
cc13446 <1344613920@qq.com>
2022-06-15 16:18:19 +0800

    173

---
+++ 173.二叉搜索树迭代器.java
@@ -0,0 +1,72 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+/*
+ * @lc app=leetcode.cn id=173 lang=java
+ *
+ * [173] 二叉搜索树迭代器
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    Queue<TreeNode> q = new LinkedList<>();
+
+    public BSTIterator(TreeNode root) {
+        Stack<TreeNode> s = new Stack<>();
+        do{
+            while(root != null) {
+                s.push(root);
+                root = root.left;
+            }
+            root = s.pop();
+            q.offer(root);
+            root = root.right;
+
+        } while(!s.isEmpty() || root != null);
+    }
+    
+    public int next() {
+        return q.poll().val;
+    }
+    
+    public boolean hasNext() {
+        return !q.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+// @lc code=end
+

a14891188b01cdafc41cefaa0a6c2407af54b418
cc13446 <1344613920@qq.com>
2022-06-19 17:03:48 +0800

    199

---
+++ 199.二叉树的右视图.java
@@ -0,0 +1,64 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+/*
+ * @lc app=leetcode.cn id=199 lang=java
+ *
+ * [199] 二叉树的右视图
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+       
+        if (root == null) return new LinkedList<>();
+        List<Integer> res = new LinkedList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
+            Queue<TreeNode> t = new LinkedList<>(q);
+            q.clear();
+            int i = 0;
+            while(!t.isEmpty()) {
+                TreeNode node = t.poll();
+                i = node.val;
+                if (node.left != null) q.offer(node.left);
+                if (node.right != null) q.offer(node.right);
+            }
+            res.add(i);
+        }
+
+        return res;
+        
+    }
+}
+// @lc code=end
+

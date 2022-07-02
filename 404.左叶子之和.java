a0b2c9553ed5d7ffba0ae08633f7a3c18ac72e8d
cc13446 <1344613920@qq.com>
2022-07-02 13:28:21 +0800

    404

---
+++ 404.左叶子之和.java
@@ -0,0 +1,44 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+/*
+ * @lc app=leetcode.cn id=404 lang=java
+ *
+ * [404] 左叶子之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null) return sumOfLeftLeaves(root.right);
+        if (root.left.left == null && root.left.right == null) return root.left.val + sumOfLeftLeaves(root.right);
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+}
+// @lc code=end
+

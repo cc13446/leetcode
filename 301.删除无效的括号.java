cde0cb11a203051d3450f408bc053927bbbae686
cc13446 <1344613920@qq.com>
2022-07-02 23:48:11 +0800

    301

---
+++ 301.删除无效的括号.java
@@ -0,0 +1,74 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=301 lang=java
+ *
+ * [301] 删除无效的括号
+ */
+
+// @lc code=start
+class Solution {
+    private List<String> res = new LinkedList<String>();
+
+    public List<String> removeInvalidParentheses(String s) {
+        int lremove = 0;
+        int rremove = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                lremove++;
+            } else if (s.charAt(i) == ')') {
+                if (lremove == 0) {
+                    rremove++;
+                } else {
+                    lremove--;
+                }
+            }
+        }
+        helper(s, 0, lremove, rremove);
+
+        return res;
+    }
+
+    private void helper(String str, int start, int lremove, int rremove) {
+        if (lremove == 0 && rremove == 0) {
+            if (isValid(str)) {
+                res.add(str);
+            }
+            return;
+        }
+
+        for (int i = start; i < str.length(); i++) {
+            if (i != start && str.charAt(i) == str.charAt(i - 1)) {
+                continue;
+            }
+            // 如果剩余的字符无法满足去掉的数量要求，直接返回
+            if (lremove + rremove > str.length() - i) {
+                return;
+            }
+            // 尝试去掉一个左括号
+            if (lremove > 0 && str.charAt(i) == '(') {
+                helper(str.substring(0, i) + str.substring(i + 1), i, lremove - 1, rremove);
+            }
+            // 尝试去掉一个右括号
+            if (rremove > 0 && str.charAt(i) == ')') {
+                helper(str.substring(0, i) + str.substring(i + 1), i, lremove, rremove - 1);
+            }
+        }
+    }
+
+    private boolean isValid(String s) {
+        int res = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+               res++;
+            } else if (c == ')') {
+                if (res == 0) return false;
+                res --;
+            }
+        }
+        return res == 0;
+    }
+}
+// @lc code=end
+

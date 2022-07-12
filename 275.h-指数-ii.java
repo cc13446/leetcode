7f8c13b2f7dbf5ae32e13a2333adb36758f24b8a
cc13446 <42083585+cc13446@users.noreply.github.com>
2022-07-12 17:29:17 +0800

    emmm 什么玩意

---
+++ 275.h-指数-ii.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=275 lang=java
+ *
+ * [275] H 指数 II
+ */
+
+// @lc code=start
+class Solution {
+    public int hIndex(int[] citations) {
+        int h = 0, i = citations.length - 1; 
+        while (i >= 0 && citations[i] > h) {
+            h++; 
+            i--;
+        }
+        return h;
+    }
+}
+// @lc code=end
+

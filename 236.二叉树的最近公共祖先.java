88926bbd688e136a4c872453ac000ce6b92f767f
cc13446 <1344613920@qq.com>
2023-02-12 11:57:12 +0800

    236

--- 236.二叉树的最近公共祖先.java
+++ 236.二叉树的最近公共祖先.java
@@ -25,48 +25,17 @@ class TreeNode {
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<List<TreeNode>> res = new ArrayList<>(2);
-        List<TreeNode> path = new ArrayList<>();
-        dfs(root, p, q, path, res);
-
-        List<TreeNode> p1 = res.get(0);
-        List<TreeNode> p2 = res.get(1);
-
-        TreeNode t = root;
-        int len = Math.min(p1.size(), p2.size());
-        for (int i = 0; i < len; i++) {
-            TreeNode t1 = p1.get(i);
-            TreeNode t2 = p2.get(i);
-            if (t1 == t2) {
-                t = t1;
-            } else {
-                break;
-            }
-        }
-        return t;
+        if (root == p || root == q || root == null) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null)
+            return root;
+        else if (right != null)
+            return right;
+        else 
+            return left;
     }
 
-    private void dfs(TreeNode root, TreeNode p, TreeNode q, List<TreeNode> path, List<List<TreeNode>> res) {
-        path.add(root);
-        if (res.size() == 2) {
-            path.remove(path.size() - 1);
-            return;
-        }
-        if (root == p || root == q) {
-            res.add(new ArrayList<>(path));
-            if (res.size() == 2){
-                path.remove(path.size() - 1);
-                return;
-            }
-        }
-        if (root.left != null) {
-            dfs(root.left, p, q, path, res);
-        }
-        if (root.right != null) {
-            dfs(root.right, p, q, path, res);
-        }
-        path.remove(path.size() - 1);
-    }
 }
 // @lc code=end
 

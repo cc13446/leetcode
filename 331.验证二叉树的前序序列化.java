4555a8df28f460dcad00a3301d1e589d081f4aa1
cc13446 <1344613920@qq.com>
2023-02-26 14:53:10 +0800

    331

--- 331.验证二叉树的前序序列化.java
+++ 331.验证二叉树的前序序列化.java
@@ -1,6 +1,3 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 /*
  * @lc app=leetcode.cn id=331 lang=java
  *
@@ -10,20 +7,22 @@ import java.util.Deque;
 // @lc code=start
 class Solution {
     public boolean isValidSerialization(String preorder) {
-        String[] s = preorder.split(",");
-        Deque<String> stack = new ArrayDeque<>(s.length);
-        for (int i = 0; i < s.length; i++) {
-            if (s[i].equals("#")) {
-                while (!stack.isEmpty() && stack.peek().equals("#")) {
-                    if (stack.size() < 2) return false;
-                    stack.pop();
-                    stack.pop();   
-                }
-            } 
-            stack.push(s[i]);
+        // 一个序列什么时候一定合法
+        // 首先 节点数量关系要满足 其次 遍历过程中 叶子节点不能分布不均
+        // ① 记录叶子节点 和 总结节点数
+        int leafCount = 0, nodeCount = 1;
+        // 左右孩子只能同时出现的二叉树必然 叶子节点数 = 非叶子节点数 + 1
+        for (char ch: preorder.toCharArray()) {
+            // ② 遍历过程中 叶子节点 和 非叶子节点的关系
+            // 这步有点难理解 这样想 我们最后遍历到的必然是 # 叶子节点
+            // 叶子节点显然会出现在序列最后 
+            // 所以在树的先序遍历过程中 (在最后一个节点前) 叶子节点最多和非叶子节点相等
+            // 如果叶子节点超过了非叶子 说明至少有一个叶子节点#上接了其他节点
+            if (leafCount > nodeCount - leafCount) return false;
+            if (ch == ',') nodeCount++;
+            if (ch == '#') leafCount++;
         }
-        return stack.size() == 1 && stack.peek().equals("#");
-
+        return (nodeCount - leafCount) + 1 == leafCount;
     }
 }
 // @lc code=end
